ecb7438a03b03d066a96794bcfeb23d964a8d5e6
tahlil <bsse0803@iit.du.ac.bd>
2022-07-20 00:40:00 +0600

    Import abi

--- scripts/stressTest.js
+++ scripts/stressTest.js
@@ -1,3 +1,6 @@
+const contract = require("../artifacts/contracts/Oracle.sol/Oracle.json");
+const abi = contract.abi;
 const PRIVATE_KEY = process.env.PRIVATE_KEY;
 const CONTRACT_ADDRESS = process.env.CONTRACT_ADDRESS;
 const RPC_API_KEY  = process.env.RPC_API_KEY || '';
+
