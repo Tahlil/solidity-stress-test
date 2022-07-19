64e47054a91f5b04e443a80f89314e15ccae6cc8
tahlil <bsse0803@iit.du.ac.bd>
2022-07-19 17:29:32 +0600

    Get leagues data

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
 require('dotenv').config();
+let request = require("request");
 
 const apiKey = process.env.API_KEY;
+
+var options = {
+  method: 'GET',
+  url: 'https://v3.football.api-sports.io/leagues',
+  headers: {
+    'x-rapidapi-host': 'v3.football.api-sports.io',
+    'x-rapidapi-key': apiKey
+  }
+};
+
+request(options, function (error, response, body) {
+	if (error) throw new Error(error);
+
+	console.log(body);
+});
