eccc201a09d95776145603d0c9af46c2a2a51312
tahlil <bsse0803@iit.du.ac.bd>
2022-07-19 18:50:35 +0600

    Aggragate all data from a season

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ const players = [
     "Rubye Baumbach","Yvonne Gaylord IV","Erling Emard","Mrs. Elwin Weber","Fern Labadie","Ardella Will","Ramon Boyle","Elyssa Hane","Opal Romaguera II","Rhett Klocko","Odie Monahan","April Ratke","Jayne Hessel","Garett DuBuque","Cyril Morar","Dedrick Bayer","Abdullah Franecki","Laverna Lemke","Hilda Wehner","Dane Crona","Miles Kshlerin","Julius Donnelly","Autumn Grady","Millie Gaylord","Jarred Nader","Anabelle Doyle","Stevie Stark","Bud West","Kellie Carter","Alfonzo Walker III","Abby Braun","Giovani Williamson","Assunta Hermann","Jace Crooks","Layne Gislason","Kiel Farrell","Arlo Blick","Cecil Abshire IV","Braxton Quigley","Elvie Simonis","Ana O'Conner","Jayde Hane","Jayne Goyette","Alvina Schimmel","Frank Dach","Carey Effertz","Jasen Morar","Wilhelm Kozey MD","Ebba Runte","Chloe Stiedemann"
 ]
 
+const getRandomNumber = (min, max) => Math.floor(Math.random()*(max-min+1)+min);
+
 const getRandomIndexes = (length, size) => {
     const indexes = [];
     const created = {};
@@ -44,11 +46,17 @@ request(options, function (error, response, body) {
     const allMatches= body.results;
 	console.log();
     let allResults = [];
-    for (let index = 0; index < array.length; index++) {
-        let fixture = "";
-        const match = body.response[index];
-        fixture += match.teams.home.name + "," + match.teams.away.name + "," + match.goals.home + "," + match.goals.away;
-        let randomPlayers = getNRandomPlayers()
+    let fixture = "";
+
+    for (let index = 0; index < allMatches.length; index++) {
+        fixture = "";
+        const match = allMatches[index];
+        fixture += match.teams.home.name + "," + match.teams.away.name + "," + match.goals.home + "," + match.goals.away + ",";
+        let goals = getRandomNumber(0, match.goals.home+match.goals.away);
+        const randomPlayers = getNRandomPlayers(goals)
+        let individualGoalsScored = Array.from({length: goals}, () => Math.floor(Math.random() * goals)).join(",");
+        fixture += (randomPlayers+","+individualGoalsScored);
+        allResults.push(fixture);
     }
-	console.log(body.response.length);
+	console.log(fixture);
 });
