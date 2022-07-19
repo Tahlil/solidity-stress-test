2dfee315707e6cdfcfba4d88b2b7ba9d3fb214fa
tahlil <bsse0803@iit.du.ac.bd>
2022-07-20 00:25:23 +0600

    Deploy smart contract

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,83 @@
-import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox";
+const dotenv = require('dotenv');
+const path = require('path');
 
-const config: HardhatUserConfig = {
-  solidity: "0.8.9",
+dotenv.config({ path: path.resolve(__dirname, '.env') });
+require("@nomiclabs/hardhat-waffle");
+// require('hardhat-deploy-ethers');
+// require('hardhat-deploy');
+// require('hardhat-typechain');
+// require('hardhat-gas-reporter');
+// require('solidity-coverage');
+// require('@typechain/ethers-v5');
+
+const chainIds = {
+  ganache: 1337,
+  goerli: 5,
+  hardhat: 31337,
+  kovan: 42,
+  mainnet: 1,
+  rinkeby: 4,
+  ropsten: 3,
 };
 
-export default config;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+const RPC_API_KEY  = process.env.RPC_API_KEY || '';
+
+
+const getRPCURL = (network: String) => {
+  return "https://eth-"+network+".alchemyapi.io/v2/"+RPC_API_KEY;
+};
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+  defaultNetwork: 'hardhat',
+  networks: {
+    ropsten: {
+      url: getRPCURL('ropsten'),
+      accounts:  [`${PRIVATE_KEY}`],
+      chainId: chainIds.ropsten,
+    },
+    rinkeby: {
+      url: getRPCURL('rinkeby'),
+      accounts: [`${PRIVATE_KEY}`],
+      chainId: chainIds.rinkeby,
+    },
+    bsctest: {
+      url: 'https://data-seed-prebsc-1-s1.binance.org:8545',
+      chainId: 97,
+      gasPrice: 20000000000,
+      accounts: [`${PRIVATE_KEY}`],
+    },
+    bscmain: {
+      url: 'https://bsc-dataseed.binance.org/',
+      chainId: 56,
+      gasPrice: 20000000000,
+      accounts: [`${PRIVATE_KEY}`],
+    },
+  },
+  solidity: {
+    compilers: [
+      {
+        version: '0.8.14',
+        settings: { optimizer: { enabled: true, runs: 200 } },
+      },
+    ],
+  },
+  gasReporter: {
+    currency: 'USD',
+    enabled: process.env.GAS_REPORT ? true : false,
+  },
+  typechain: {
+    outDir: 'src/typechain',
+  },
+  namedAccounts: {
+    deployer: {
+      default: 0, // here this will by default take the first account as deployer
+    },
+  },
+  paths: {
+    deployments: 'src/deployments',
+  }
+};
--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,16 +1,16 @@
-import { ethers } from "hardhat";
-
+const { ethers } = require("hardhat");
 async function main() {
   const Oracle = await ethers.getContractFactory("Oracle");
   const oracle = await Oracle.deploy();
 
   const tx = await oracle.deployed();
-  console.log(tx);
+  console.log("Deployed address", tx.address);
+  const estimatedGas = await ethers.provider.estimateGas(tx.deployTransaction.data)
+  console.log("Estimated Gas", estimatedGas);
   
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
+
 main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
