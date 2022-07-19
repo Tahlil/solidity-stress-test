7120a6ea810bbd1080110c6e2621da55274d0267
tahlil <bsse0803@iit.du.ac.bd>
2022-07-19 21:25:52 +0600

    Add data scructure

--- contracts/Oracle.sol
+++ contracts/Oracle.sol
@@ -4,11 +4,25 @@ pragma solidity ^0.8.14;
 // Import this file to use console.log
 import "hardhat/console.sol";
 
-contract Lock {
-    address private owner;
+contract Oracle {
+    address private _owner;
+    struct Match{
+        string homeTeam;
+        string awayTeam;
+        uint8 homeGoal;
+        uint8 awayGoal;
+        string[] goalScorers;
+        uint8[] numberOfGoalsScored;
+    }
+    
+/*
+* @author Tahlil
+* @dev string -> <LeagueName>-<YYYY> (YYYY -> season i. e. for 2019-2020 session - session=2019)
+*/
+    mapping (string=>Match[]) private _matches;
 
-    constructor() payable {
-        owner = msg.sender;
+    constructor() {
+        _owner = msg.sender;
     }
 
 }
