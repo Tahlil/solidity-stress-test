0df54e5a616198c0d4e7ac94d443cfa56cb6ee0b
tahlil <bsse0803@iit.du.ac.bd>
2022-07-19 18:18:12 +0600

    Get overall score of each match

--- index.js
+++ index.js
@@ -5,15 +5,27 @@ const apiKey = process.env.API_KEY;
 
 var options = {
   method: 'GET',
-  url: 'https://v3.football.api-sports.io/leagues',
+  url: 'https://v3.football.api-sports.io/fixtures',
   headers: {
     'x-rapidapi-host': 'v3.football.api-sports.io',
     'x-rapidapi-key': apiKey
-  }
+  },
+  qs: {
+      'league':140,
+      'season': 2021
+  },
+  json: true
 };
 
 request(options, function (error, response, body) {
 	if (error) throw new Error(error);
-
-	console.log(body);
+    const allMatches= body.results;
+	console.log();
+    let allResults = [];
+    for (let index = 0; index < array.length; index++) {
+        let fixture = "";
+        const match = body.response[index];
+        fixture += match.teams.home.name + "," + match.teams.away.name + "," + match.goals.home + "," + match.goals.away;
+    }
+	console.log(body.response.length);
 });
