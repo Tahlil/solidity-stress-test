24a7d500ad5735146a758ad560aa35ad55d6e306
tahlil <bsse0803@iit.du.ac.bd>
2022-07-20 00:43:59 +0600

    Create contract instance

--- scripts/stressTest.js
+++ scripts/stressTest.js
@@ -1,6 +1,16 @@
+const { ethers } = require("hardhat");
 const contract = require("../artifacts/contracts/Oracle.sol/Oracle.json");
 const abi = contract.abi;
 const PRIVATE_KEY = process.env.PRIVATE_KEY;
 const CONTRACT_ADDRESS = process.env.CONTRACT_ADDRESS;
 const RPC_API_KEY  = process.env.RPC_API_KEY || '';
+
+// Provider
+const alchemyProvider = new ethers.providers.AlchemyProvider(network="rinkeby", RPC_API_KEY);
+
+// Signer
+const signer = new ethers.Wallet(PRIVATE_KEY, alchemyProvider);
+
+// Contract
+const oracleContract = new ethers.Contract(CONTRACT_ADDRESS, abi, signer);
 
